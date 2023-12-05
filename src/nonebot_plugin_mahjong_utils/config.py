6e0f90f2413da3b0c3969e797e91406302a6c591
ssttkkl <huang.wen.long@hotmail.com>
2023-12-05 12:43:42 +0800

    接入mahjong-utils 0.6.1

--- src/nonebot_plugin_mahjong_utils/config.py
+++ src/nonebot_plugin_mahjong_utils/config.py
@@ -1,12 +1,19 @@
+from pathlib import Path
+from typing import Optional
+
+from mahjong_utils.bridge.webapi_jar.path import set_mahjongutils_webapi_jar_path
 from nonebot import get_driver
 from pydantic import BaseSettings
 
 
 class Config(BaseSettings):
     mahjong_utils_send_image: bool = False
+    mahjong_utils_webapi_jar: Optional[str]
 
     class Config:
         extra = "ignore"
 
 
 conf = Config(**get_driver().config.dict())
+if conf.mahjong_utils_webapi_jar:
+    set_mahjongutils_webapi_jar_path(Path(conf.mahjong_utils_webapi_jar))
