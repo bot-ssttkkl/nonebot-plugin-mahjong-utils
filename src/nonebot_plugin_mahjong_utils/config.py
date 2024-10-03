0780e6abe38d8c33663509f660e58d596958a21f
ssttkkl <huang.wen.long@hotmail.com>
2024-10-03 17:18:33 +0800

    去除webapi_jar的配置项

--- src/nonebot_plugin_mahjong_utils/config.py
+++ src/nonebot_plugin_mahjong_utils/config.py
@@ -1,14 +1,9 @@
-from pathlib import Path
-from typing import Optional
-
-from mahjong_utils.bridge.webapi_jar.path import set_mahjongutils_webapi_jar_path
 from nonebot import get_driver
 from pydantic import BaseSettings
 
 
 class Config(BaseSettings):
     mahjong_utils_send_image: bool = True
-    mahjong_utils_webapi_jar: Optional[str]
 
     mahjong_utils_test: bool = False
 
@@ -17,5 +12,3 @@ class Config(BaseSettings):
 
 
 conf = Config(**get_driver().config.dict())
-if conf.mahjong_utils_webapi_jar:
-    set_mahjongutils_webapi_jar_path(Path(conf.mahjong_utils_webapi_jar))
